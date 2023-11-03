f647997df6f3e45d6f7cf7b2b9ead3563183d629
juber <juberk1010@gmail.com>
2023-11-03 10:45:40 +0530

    make dom project using css

---
+++ script.js
@@ -0,0 +1,8 @@
+const div = document.getElementById('container');
+div.style.backgroundColor = "yellow";
+div.style.margin = "20px";
+div.style.padding = "20px";
+div.style.fontSize = "18px";
+div.style.fontWeight = "bold";
+div.style.height = "200px";
+div.style.width = "300px";
